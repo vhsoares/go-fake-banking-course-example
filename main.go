b5c5e70fe157d44dacf068a349cfbe2f0eadcafe
vhsoares <vsbbbeico@gmail.com>
2020-07-22 14:59:02 -0300

    fixing logic publishing funcionality

--- main.go
+++ main.go
@@ -9,8 +9,20 @@ import (
 // classe para exibição do funcionamento do pacote de Conta
 func main() {
 	firstAccount := accounts.Account{"Vitor", 1255, accounts.Saldo{3000., 600.}, "None"}
+	secondAccount := accounts.Account{"Vitor", 1255, accounts.Saldo{3000., 600.}, "None"}
+
+	accountVerification(firstAccount, secondAccount)
 
 	firstAccount.Deposit(200)
+	accountVerification(firstAccount, secondAccount)
 
-	fmt.Println(firstAccount)
+	secondAccount.Draw(500)
+	accountVerification(firstAccount, secondAccount)
+
+	firstAccount.Transfer(1000, &secondAccount)
+	accountVerification(firstAccount, secondAccount)
+}
+
+func accountVerification(firstAccount accounts.Account, secondAccount accounts.Account) {
+	fmt.Println("Conta 1: ", firstAccount, "Conta 2: ", secondAccount)
 }
